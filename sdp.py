c98d344eddb2d3490a00bca6d9c2deff3d7f83cc
Marek Goldmann <marek.goldmann@gmail.com>
2013-10-23 13:42:29 +0200

    Remove the ke-release-event, disable time update. Needs more work on threading. Added license header.

--- sdp.py
+++ sdp.py
@@ -1,3 +1,22 @@
+# Copyright (c) 2013 Marek Goldmann
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of
+# this software and associated documentation files (the "Software"), to deal in
+# the Software without restriction, including without limitation the rights to
+# use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+# the Software, and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+# FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+# COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+# IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 from gi.repository import Gtk, GdkPixbuf, GObject
 #from gi.repository import Gst as gst
 import threading
@@ -124,9 +143,9 @@ class MainWindow:
 #      s = sched.scheduler(time.time, time.sleep)
 #      s.enter(60, 1, self.update_time, (s,))
 
-      th_st = threading.Thread(target=self.update_time)
-      th_st.daemon = True
-      th_st.start()
+#      th_st = threading.Thread(target=self.update_time)
+#      th_st.daemon = True
+#      th_st.start()
 #      s.run()
 
 
